0c61c322691f35ac5b7e3ba4d24fbc1efd67fb6e
Artur Candeia <arturr.candeia@gmail.com>
2023-03-21 19:47:42 -0300

    Adiciona telas informativas

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 void tela_inicial(void);
-void tela_sgobre(void);
-void tela_info(void);
+void tela_sobre(void);
+void tela_equipe(void);
 
 int main(void) {
   tela_inicial();
   tela_sobre();
-  tela_info();
+  tela_equipe();
   return 0;
 
 }
@@ -22,6 +22,35 @@ void tela_inicial(void) {
     printf("===            3. Módulo                                                    ===\n");
     printf("===            4. Módulo                                                    ===\n");
     printf("===            0. Sair                                                      ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
+    printf("\n");
+}
+
+void tela_sobre(void) {
+    printf("\n");
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===             Universidade Federal do Rio Grande do Norte                 ===\n");
+    printf("===                 Centro de Ensino Superior do Seridó                     ===\n");
+    printf("===               Departamento de Computação e Tecnologia                   ===\n");
+    printf("===                  Disciplina DCT1106 -- Programação                      ===\n");
+    printf("===                  Projeto Sistema de Gerenciamento Clinico               ===\n");
+    printf("===             Developed by @ArturCandeia -- since Mar, 2023               ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
+}
+
+void tela_equipe(void) {
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===            = = Sistema de de Gerenciamento Clinico = =                  ===\n");
+    printf("===                                                                         ===\n");
+    printf("===            Este projeto foi desenvolvido por:                           ===\n");
+    printf("===                                                                         ===\n");
+    printf("===            nome: Artur Morais Candeia                                   ===\n");
+    printf("===            E-mail: arturr.candeia@gmail.com                             ===\n");
+    printf("===            Git: https://github.com/ArturCandeia/Projeto-SIG-Clinic.git  ===\n");
     printf("===                                                                         ===\n");
     printf("===============================================================================\n");
     printf("\n");
