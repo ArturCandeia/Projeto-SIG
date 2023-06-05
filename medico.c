0025ffa7706d6168932a27934595312602161346
Artur Candeia <arturr.candeia@gmail.com>
2023-06-05 11:25:32 -0300

    implemente listagem e arquivação de dados

--- medico.c
+++ medico.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include "medico.h"
+#include "verifica.h"
 
 void modulo_medico(){
   char opcao;
@@ -25,20 +26,55 @@ void modulo_medico(){
 void medico_cadastrar(void){
     Medico *med;
     med = tela_medico_cadastrar();
+    gravar_medico(med);
 
     free(med);
 }
 
 void medico_buscar(void){
-    tela_medico_buscar();
+    Medico* med;
+    char* crm;
+
+    crm = tela_medico_buscar();
+    med = achar_medico(crm);
+    exibe_medico(med);
+    free(med);
+    free(crm);
 }
 
-void medico_editar(){
-    tela_medico_editar();
+void medico_editar(void) {
+    Medico* med;
+    char* crm;
+
+    crm = tela_medico_editar();
+    med = achar_medico(crm);
+    if (med == NULL){
+        printf("Ocorreu um erro \n");
+        printf("Medico não encontrado\n");
+    } else {
+        med = tela_medico_cadastrar();
+        strcpy(med->crm, crm);
+        regravar_medico(med);
+        free(med);
+    }
+    free(crm);
 }
 
-void medico_excluir(){
-    tela_medico_excluir();
+void medico_excluir(void) {
+    Medico* med;
+    char *crm;
+
+    crm = tela_medico_excluir();
+    med = (Medico*) malloc(sizeof(Medico));
+    med = achar_medico(crm);
+    if (med == NULL) {
+        printf("Medico inexistente \n");
+    } else { 
+        med->status = 0;
+        regravar_medico(med);
+        free(med);
+    }
+    free(crm);
 }
 
 char tela_medico(void) {
@@ -80,7 +116,7 @@ Medico *tela_medico_cadastrar(void) {
     printf("===                     Nome Completo:");
     scanf("%[A-ZÁÃÂÉẼÊÍĨÎÓÕÔ a-záãâéẽêíĩîóõô]",med->nome);
     getchar();
-    printf("===                     CPF:");
+    printf("===                     crm:");
     scanf("%[0-9]",med->crm);
     getchar();
     printf("===                     E-mail:");
@@ -89,6 +125,7 @@ Medico *tela_medico_cadastrar(void) {
     printf("===                     Número de contato:");
     scanf("%[0-9]",med->phone);
     getchar();
+    med->status = True;
     printf("===                                                                         ===\n");
     printf("===============================================================================\n");
     printf("\n");
@@ -97,53 +134,159 @@ Medico *tela_medico_cadastrar(void) {
     return(med);
 }
 
-void tela_medico_buscar(void) {
-    char cpf[12];
+char* tela_medico_buscar(void) {
+    char* crm;
+
+    crm = (char*) malloc(10*sizeof(crm));
     system("clear||cls");
     printf("===============================================================================\n");
     printf("===                                                                         ===\n");
     printf("===                    = =  Busca Médico = =                                ===\n");
     printf("===                                                                         ===\n");
-    printf("===                      CPF:");
-    scanf("%[0-9]",cpf);
+    printf("===                      crm:");
+    scanf("%[0-9]",crm);
     getchar();
     printf("===                                                                         ===\n");
     printf("===============================================================================\n");
     printf("\n");
     printf("Aperte <ENTER> para continuar");
     getchar();
+    return crm;
 }
 
-void tela_medico_editar(void) {
-    char cpf[12];
+char* tela_medico_editar(void) {
+    char* crm;
+
+    crm = (char*) malloc(10*sizeof(crm));
     system("clear||cls");
     printf("===============================================================================\n");
     printf("===                                                                         ===\n");
     printf("===                    = =  Edita Médico = =                                ===\n");
     printf("===                                                                         ===\n");
-    printf("===                      CPF:");
-    scanf("%[0-9]",cpf);
+    printf("===                      crm:");
+    scanf("%[0-9]",crm);
     getchar();
     printf("===                                                                         ===\n");
     printf("===============================================================================\n");
     printf("\n");
     printf("Aperte <ENTER> para continuar");
     getchar();
+    return crm;
 }
 
-void tela_medico_excluir(void) {
-    char cpf[12];
+char* tela_medico_excluir(void) {
+    char* crm;
+
+    crm = (char*) malloc(10*sizeof(crm));
     system("clear||cls");
     printf("===============================================================================\n");
     printf("===                                                                         ===\n");
     printf("===                    = =  Exclui Médico = =                               ===\n");
     printf("===                                                                         ===\n");
-    printf("===                      CPF:");
-    scanf("%[0-9]",cpf);
+    printf("===                      crm:");
+    scanf("%[0-9]",crm);
     getchar();
     printf("===                                                                         ===\n");
     printf("===============================================================================\n");
     printf("\n");
     printf("Aperte <ENTER> para continuar");
     getchar();
+    return crm;
+}
+
+void tela_error_medico(void){
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===                Houve um erro ao acessar o arquivo                       ===\n");
+    printf("===                Tente novamente!                                         ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
+}
+
+void exibe_medico(Medico *med) {
+    if (med == NULL) {
+        printf("Medico não encontrado \n");
+    } else {
+        system("clear||cls");
+        printf("=Dados do Medico= \n");
+        printf("Nome: %s\n", med->nome);
+        printf("crm: %s\n", med->crm);
+        printf("email: %s\n", med->email);
+        printf("Telefone: %s\n", med->phone);
+        printf("status %d\n", med->status);
+    }
+    printf("Aperte <ENTER> para continuar \n");
+    getchar();
+}
+
+void gravar_medico(Medico* med){
+    FILE* fp;
+
+    fp = fopen("medico.dat","ab");
+    if (fp == NULL) {
+         tela_error_medico();
+    }
+    fwrite(med, sizeof(Medico), 1, fp);
+    fclose(fp);
+}
+
+void regravar_medico(Medico* med) {
+    int encontrado;
+    FILE* fp;
+    Medico* medVisto;
+
+    medVisto = (Medico*) malloc(sizeof(Medico));
+    fp = fopen("medico.dat","r+b");
+    if(fp == NULL) {
+        tela_error_medico();
+    }
+    encontrado = False;
+    while(fread(medVisto, sizeof(Medico), 1, fp) && !encontrado){
+        if (strcmp(medVisto->crm, med->crm)==0){
+            encontrado = True;
+            fseek(fp, -1*sizeof(Medico), SEEK_CUR);
+        fwrite(med, sizeof(Medico), 1, fp);
+        }
+    }
+    fclose(fp);
+    free(medVisto);
+}
+
+Medico* achar_medico(char* crm) {
+    FILE* fp;
+    Medico* med;
+
+    med = (Medico*) malloc(sizeof(Medico));
+    fp = fopen("medico.dat","rb");
+    if (fp == NULL) {
+        tela_error_medico();
+    }
+    while(fread(med, sizeof(Medico), 1,fp)) {
+        if ((strcmp(med->crm, crm)== 0) && (med->status == True)) {
+            fclose(fp);
+            return med;
+        }
+    }
+    fclose(fp);
+    return NULL;
+}
+
+void lista_medicos(void){
+    FILE *fp;
+    Medico* med;
+
+    med = (Medico*) malloc(sizeof(Medico));
+    fp = fopen("medico.dat","rb");
+    while (fread(med,sizeof(Medico),1,fp)) {
+        printf("\n =Dados do Medico= \n\n");
+        printf("Nome: %s\n", med->nome);
+        printf("crm: %s\n", med->crm);
+        printf("email: %s\n", med->email);
+        printf("Telefone: %s\n", med->phone);
+        printf("status %d\n\n", med->status);
+    }
+    printf("Aperte ENTER para continuar \n");
+    getchar();
+    fclose(fp);
+    free(med);
 }
